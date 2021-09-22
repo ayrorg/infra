ea834180cef17ad55e466fb6858a1a0317a461b0
Simen A. W. Olsen <so@bjerk.io>
2021-09-22 23:33:38 +0200

    remove reference to console service account

--- index.ts
+++ index.ts
@@ -2,6 +2,3 @@ import './resources/console';
 import './resources/github';
 import './resources/google';
 import './resources/website';
-import { serviceAccountKey } from './resources/console/google/deploy-service-account'
-
-export const key = serviceAccountKey.privateKeyData;
