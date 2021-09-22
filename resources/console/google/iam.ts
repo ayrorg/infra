7135bb244ae0e0937cde208463d1a020acdc968c
Simen A. W. Olsen <so@bjerk.io>
2021-09-22 18:32:14 +0200

    bump core deployer to owner rights

--- resources/console/google/iam.ts
+++ resources/console/google/iam.ts
@@ -4,30 +4,34 @@ import { viewerUsers } from '../config';
 import { consoleProject } from './project';
 import { deployServiceAccountEmail } from '../../config';
 
-export const projectIamPolicy =
-  new google.cloudresourcemanager.v1.ProjectIamPolicy(
-    'console-iam-policy',
-    {
-      resource: consoleProject.projectId,
-      bindings: [
-        {
-          members: viewerUsers.map((u) => interpolate`user:${u}`),
-          role: 'roles/viewer',
-        },
-        {
-          members: [
-            'serviceAccount:service-702460706354@gcf-admin-robot.iam.gserviceaccount.com',
-          ],
-          role: 'roles/cloudfunctions.serviceAgent',
-        },
-        {
-          members: [
-            'serviceAccount:451937147092@cloudservices.gserviceaccount.com',
-            interpolate`serviceAccount:${deployServiceAccountEmail}`,
-          ],
-          role: 'roles/editor',
-        },
-      ],
-    },
-    { dependsOn: consoleProject },
-  );
+export const projectIamPolicy = new google.cloudresourcemanager.v1.ProjectIamPolicy(
+  'console-iam-policy',
+  {
+    resource: consoleProject.projectId,
+    bindings: [
+      {
+        members: viewerUsers.map((u) => interpolate`user:${u}`),
+        role: 'roles/viewer',
+      },
+      {
+        members: [
+          'serviceAccount:service-702460706354@gcf-admin-robot.iam.gserviceaccount.com',
+        ],
+        role: 'roles/cloudfunctions.serviceAgent',
+      },
+      {
+        members: [
+          'serviceAccount:451937147092@cloudservices.gserviceaccount.com',
+        ],
+        role: 'roles/editor',
+      },
+      {
+        members: [
+          interpolate`serviceAccount:${deployServiceAccountEmail}`,
+        ],
+        role: 'roles/owner',
+      },
+    ],
+  },
+  { dependsOn: consoleProject },
+);
