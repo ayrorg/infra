3ae9ccb040e0de8aa7740ea3e28e28da9d7756f3
Simen A. W. Olsen <so@bjerk.io>
2021-12-12 22:08:17 +0100

    add iam to onboarding project

--- resources/onboarding/index.ts
+++ resources/onboarding/index.ts
@@ -1,6 +1,7 @@
-import * as githubSecrets from './github/secrets'
+import * as githubSecrets from './github/secrets';
 import './google/app-engine';
 import './google/deploy-service-account';
 import './google/project';
+import './google/iam';
 
 export { githubSecrets };
