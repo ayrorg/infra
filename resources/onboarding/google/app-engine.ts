d720c43da8374122f5f01e11f299384d481e8fff
Simen A. W. Olsen <so@bjerk.io>
2021-12-12 21:26:17 +0100

    fix(onboarding): add provider and gcp project dependency to app engine resource

--- resources/onboarding/google/app-engine.ts
+++ resources/onboarding/google/app-engine.ts
@@ -1,6 +1,12 @@
 import * as gcp from '@pulumi/gcp';
+import { provider } from './provider';
+import { project } from './project';
 import { appEngineLocation } from '../config';
 
-export const appEngine = new gcp.appengine.Application('onboarding', {
-  locationId: appEngineLocation,
-});
+export const appEngine = new gcp.appengine.Application(
+  'onboarding',
+  {
+    locationId: appEngineLocation,
+  },
+  { provider, dependsOn: project },
+);
