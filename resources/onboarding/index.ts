b981ce02b8abcc98a83462cc390e6d0d36dffb02
Simen A. W. Olsen <so@bjerk.io>
2021-12-12 21:38:29 +0100

    remove app-engine

--- resources/onboarding/index.ts
+++ resources/onboarding/index.ts
@@ -1,5 +1,5 @@
 import * as githubSecrets from './github/secrets'
-import './google/app-engine';
+// import './google/app-engine';
 import './google/deploy-service-account';
 import './google/project';
 
