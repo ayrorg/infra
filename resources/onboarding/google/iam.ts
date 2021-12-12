4ce1440c73ceee83baf9146372357810dcc8a913
Simen A. W. Olsen <so@bjerk.io>
2021-12-12 22:26:24 +0100

    give more access to onboarding deploy service account

--- resources/onboarding/google/iam.ts
+++ resources/onboarding/google/iam.ts
@@ -5,73 +5,88 @@ import { project } from './project';
 import { deployServiceAccountEmail } from '../../config';
 import { serviceAccount as deployServiceAccount } from './deploy-service-account';
 
-export const projectIamPolicy =
-  new google.cloudresourcemanager.v1.ProjectIamPolicy(
-    'onboarding-iam-policy',
-    {
-      resource: project.projectId,
-      bindings: [
-        {
-          members: viewerUsers.map((u) => interpolate`user:${u}`),
-          role: 'roles/viewer',
-        },
-        {
-          members: [
-            interpolate`serviceAccount:${project.number}@cloudservices.gserviceaccount.com`,
-            interpolate`serviceAccount:${project.number}-compute@developer.gserviceaccount.com`,
-            interpolate`serviceAccount:service-${project.number}@containerregistry.iam.gserviceaccount.com`,
-          ],
-          role: 'roles/editor',
-        },
-        {
-          members: [
-            interpolate`serviceAccount:${deployServiceAccountEmail}`, // Core deployment service account
-            'user:so@ayr.no',
-          ],
-          role: 'roles/owner',
-        },
-        // Google-provided role grants
-        {
-          members: [
-            interpolate`serviceAccount:${project.number}@cloudbuild.gserviceaccount.com`,
-          ],
-          role: 'roles/cloudbuild.builds.builder',
-        },
-        {
-          members: [
-            interpolate`serviceAccount:service-${project.number}@gcp-sa-cloudbuild.iam.gserviceaccount.com`,
-          ],
-          role: 'roles/cloudbuild.serviceAgent',
-        },
-        {
-          members: [
-            interpolate`serviceAccount:service-${project.number}@gcf-admin-robot.iam.gserviceaccount.com`,
-          ],
-          role: 'roles/cloudfunctions.serviceAgent',
-        },
-        {
-          members: [
-            interpolate`serviceAccount:service-${project.number}@compute-system.iam.gserviceaccount.com`,
-          ],
-          role: 'roles/compute.serviceAgent',
-        },
-        {
-          members: [
-            interpolate`serviceAccount:service-${project.number}@container-engine-robot.iam.gserviceaccount.com`,
-          ],
-          role: 'roles/container.serviceAgent',
-        },
-        {
-          members: [interpolate`serviceAccount:${deployServiceAccount.email}`],
-          role: 'roles/appengine.appAdmin',
-        },
-        {
-          members: [
-            interpolate`serviceAccount:${project.number}-compute@developer.gserviceaccount.com`,
-          ],
-          role: 'roles/cloudtrace.agent',
-        },
-      ],
-    },
-    { dependsOn: project },
-  );
+export const projectIamPolicy = new google.cloudresourcemanager.v1.ProjectIamPolicy(
+  'onboarding-iam-policy',
+  {
+    resource: project.projectId,
+    bindings: [
+      {
+        members: viewerUsers.map((u) => interpolate`user:${u}`),
+        role: 'roles/viewer',
+      },
+      {
+        members: [
+          interpolate`serviceAccount:${project.number}@cloudservices.gserviceaccount.com`,
+          interpolate`serviceAccount:${project.number}-compute@developer.gserviceaccount.com`,
+          interpolate`serviceAccount:service-${project.number}@containerregistry.iam.gserviceaccount.com`,
+        ],
+        role: 'roles/editor',
+      },
+      {
+        members: [
+          interpolate`serviceAccount:${deployServiceAccountEmail}`, // Core deployment service account
+          'user:so@ayr.no',
+        ],
+        role: 'roles/owner',
+      },
+      // Google-provided role grants
+      {
+        members: [
+          interpolate`serviceAccount:${project.number}@cloudbuild.gserviceaccount.com`,
+        ],
+        role: 'roles/cloudbuild.builds.builder',
+      },
+      {
+        members: [
+          interpolate`serviceAccount:service-${project.number}@gcp-sa-cloudbuild.iam.gserviceaccount.com`,
+        ],
+        role: 'roles/cloudbuild.serviceAgent',
+      },
+      {
+        members: [
+          interpolate`serviceAccount:service-${project.number}@gcf-admin-robot.iam.gserviceaccount.com`,
+        ],
+        role: 'roles/cloudfunctions.serviceAgent',
+      },
+      {
+        members: [
+          interpolate`serviceAccount:service-${project.number}@compute-system.iam.gserviceaccount.com`,
+        ],
+        role: 'roles/compute.serviceAgent',
+      },
+      {
+        members: [
+          interpolate`serviceAccount:service-${project.number}@container-engine-robot.iam.gserviceaccount.com`,
+        ],
+        role: 'roles/container.serviceAgent',
+      },
+      {
+        members: [interpolate`serviceAccount:${deployServiceAccount.email}`],
+        role: 'roles/appengine.appAdmin',
+      },
+      {
+        members: [interpolate`serviceAccount:${deployServiceAccount.email}`],
+        role: 'roles/iam.serviceAccountUser',
+      },
+      {
+        members: [interpolate`serviceAccount:${deployServiceAccount.email}`],
+        role: 'roles/compute.storageAdmin',
+      },
+      {
+        members: [interpolate`serviceAccount:${deployServiceAccount.email}`],
+        role: 'roles/cloudbuild.builds.editor',
+      },
+      {
+        members: [interpolate`serviceAccount:${deployServiceAccount.email}`],
+        role: 'roles/cloudscheduler.admin',
+      },
+      {
+        members: [
+          interpolate`serviceAccount:${project.number}-compute@developer.gserviceaccount.com`,
+        ],
+        role: 'roles/cloudtrace.agent',
+      },
+    ],
+  },
+  { dependsOn: project },
+);
