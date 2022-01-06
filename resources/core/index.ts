6bccd186b4df7ebf276346c52cf50891c075dadf
Simen A. W. Olsen <so@bjerk.io>
2022-01-06 16:09:31 +0100

    add sql and secrets to imports

--- resources/core/index.ts
+++ resources/core/index.ts
@@ -3,5 +3,7 @@ import './google/app-engine';
 import './google/deploy-service-account';
 import './google/project';
 import './google/iam';
+import './google/secrets';
+import './google/sql';
 
 export { githubSecrets };
