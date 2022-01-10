48edd8c894de76066400e314352d9646e44c45b2
Simen A. W. Olsen <cobraz@cobraz.no>
2022-01-10 15:07:39 +0100

    Give all sql-users access to Cloud SQL (#17)

--- resources/core/config.ts
+++ resources/core/config.ts
@@ -5,4 +5,5 @@ const config = new pulumi.Config('core');
 export const projectName = config.require('project');
 export const repository = config.require('repo');
 export const viewerUsers = config.requireObject<string[]>('viewer-users');
+export const sqlUsers = config.requireObject<string[]>('sql-users');
 export const appEngineLocation = 'europe-central2';
--- resources/core/google/iam.ts
+++ resources/core/google/iam.ts
@@ -1,6 +1,6 @@
 import * as google from '@pulumi/google-native';
 import { interpolate } from '@pulumi/pulumi';
-import { viewerUsers } from '../config';
+import { viewerUsers, sqlUsers } from '../config';
 import { project } from './project';
 import { deployServiceAccountEmail } from '../../config';
 import { serviceAccount as deployServiceAccount } from './deploy-service-account';
@@ -14,6 +14,10 @@ export const projectIamPolicy =
         {
           members: viewerUsers.map((u) => interpolate`user:${u}`),
           role: 'roles/viewer',
+        },
+        {
+          members: sqlUsers.map((u) => interpolate`user:${u}`),
+          role: 'roles/cloudsql.client',
         },
         {
           members: [
