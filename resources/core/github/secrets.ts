33750e4638d526aebdb405f52a011910245d2649
Simen A. W. Olsen <cobraz@cobraz.no>
2022-01-11 10:00:24 +0100

    Add backoffice

--- resources/core/github/secrets.ts
+++ resources/core/github/secrets.ts
@@ -23,3 +23,24 @@ export const githubGcpSaKey = new github.ActionsSecret(
   },
   { provider, deleteBeforeReplace: true },
 );
+
+
+export const githubBackofficeGcpSaProject = new github.ActionsSecret(
+  `core-backoffice-gcp-project`,
+  {
+    repository: 'backoffice',
+    secretName: 'GOOGLE_PROJECT_ID',
+    plaintextValue: project.projectId,
+  },
+  { provider, deleteBeforeReplace: true },
+);
+
+export const githubBackofficeGcpSaKey = new github.ActionsSecret(
+  `core-backoffice-gcp-sa-key`,
+  {
+    repository: 'backoffice',
+    secretName: 'GOOGLE_PROJECT_SA_KEY',
+    plaintextValue: serviceAccountKey.privateKey,
+  },
+  { provider, deleteBeforeReplace: true },
+);
