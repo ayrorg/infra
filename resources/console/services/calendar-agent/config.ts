675d2c366a05d37d347082c01c435f8ca2502da1
Simen A. W. Olsen <so@bjerk.io>
2022-11-29 23:27:31 +0100

    add missing configs

--- resources/console/services/calendar-agent/config.ts
+++ resources/console/services/calendar-agent/config.ts
@@ -1,9 +1,9 @@
 import * as pulumi from '@pulumi/pulumi';
 
-const config = new pulumi.Config('secret-calendar-agent');
+const config = new pulumi.Config('calendar-agent');
 
-export const name = 'secret-calendar-agent';
+export const name = 'calendar-agent';
 export const tag = config.require('tag');
 export const imageName = config.get('image-name') ?? name;
 export const location = config.get('location') ?? 'europe-north1';
-export const domain = config.get('domain') ?? 'api.v1.ayr.no';
+export const domain = config.get('domain') ?? 'calendar-agent.v1.ayr.no';
