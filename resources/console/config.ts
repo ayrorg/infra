67be93d5f3fdf206f770ea4e1342aa6b68bd3bf9
Brage Sekse Aarset <brage.aarset@gmail.com>
2023-01-06 13:32:11 +0200

    feat: add me as developer (#47)

--- resources/console/config.ts
+++ resources/console/config.ts
@@ -6,7 +6,7 @@ export const project = config.require('project');
 export const viewerUsers = config.requireObject<string[]>('viewer-users');
 export const location = config.get('location') ?? 'europe-west1';
 export const region = config.get('location') ?? 'europe-north1';
-export const developers = ['so@bjerk.io'];
+export const developers = ['so@bjerk.io', 'brage@bjerk.io'];
 export const posthogApiKey = config.require('posthog-api-key');
 export const posthogHost = config.require('posthog-host');
 
