0dc9808f79d100aee4442af85c239123206c345e
Simen A. W. Olsen <so@bjerk.io>
2023-02-12 12:30:03 +0100

    change to autopilot

--- resources/google/gke.ts
+++ resources/google/gke.ts
@@ -8,40 +8,12 @@ const serverConfig = google.container.v1.getServerConfigOutput({
 
 const engineVersion = serverConfig.apply((conf) => conf.validMasterVersions[0]);
 
-const nodeConfig: google.types.input.container.v1.NodeConfigArgs = {
-  machineType: 'n1-standard-2',
-  oauthScopes: [
-    'https://www.googleapis.com/auth/compute',
-    'https://www.googleapis.com/auth/devstorage.read_only',
-    'https://www.googleapis.com/auth/logging.write',
-    'https://www.googleapis.com/auth/monitoring',
-  ],
-  preemptible: true,
-};
-
 export const cluster = new google.container.v1.Cluster(
   'main-cluster',
   {
+    name: 'ayr-main-cluster',
     initialClusterVersion: engineVersion,
-    nodePools: [{ initialNodeCount: 1, name: 'initial' }],
+    autopilot: { enabled: true },
   },
   { provider: provider.google },
 );
-
-new google.container.v1.NodePool(
-  'main-nodepool',
-  {
-    name: 'default-nodepool',
-    clusterId: cluster.name,
-    initialNodeCount: 1,
-    config: nodeConfig,
-    management: {
-      autoRepair: false,
-      autoUpgrade: false,
-    },
-  },
-  { provider: provider.google },
-);
-
-// export const nodepoolTag = nodepool.config.tags[0];
-// export const taintsKey = nodepool.config.taints[0].key;
